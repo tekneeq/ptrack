7a90d757c7af7aad6b896cd432ebcc5177bd3d5a
Eugene Park <eugenepark3@gmail.com>
2022-11-03 14:03:23 -0400

    googld analytics to index.html

--- bin/run.sh
+++ bin/run.sh
@@ -7,13 +7,18 @@ flask --app ptrack --debug run --host=0.0.0.0
 
 # flask --app ptrack init-db
 
-
+# pip install -r requirements.txt
 
 # pytest
 # coverage run -m pytest
 # coverage report
 # coverage html
 # pytest tests/test_item.py -k test_index -s
+
+
+# pip3 install waitress
+# waitress-serve --call 'ptrack:create_app'
+# [ec2-user@ip-172-31-91-98 ptrack]$ waitress-serve --port 5000 --call 'ptrack:create_app
 
 
 # deactivate
@@ -25,8 +30,5 @@ flask --app ptrack --debug run --host=0.0.0.0
 
 # {project name}-{version}-{python tag} -{abi tag}-{platform tag}
 # ptrack-1.0.0-py3-none-any.whl
-
-
-# waitress-serve --call 'ptrack:create_app'
 
 #python -c 'import secrets; print(secrets.token_hex())'
