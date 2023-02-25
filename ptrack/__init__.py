f290a558c2be5e73f9853fe92d427ffc91f80a32
Eugene Park <eugenepark3@gmail.com>
2023-02-25 10:43:05 -0500

    pupper

--- ptrack/__init__.py
+++ ptrack/__init__.py
@@ -9,8 +9,7 @@ from pymongo import MongoClient
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True, static_url_path='ptrack',
-                static_folder='static')
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         DATABASE=os.path.join(app.instance_path, 'ptrack.sqlite'),
         SECRET_KEY='dev'
