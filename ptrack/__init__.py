fd6e803383ab243e871e55ce7805072582aa9985
Eugene Park <eugenepark3@gmail.com>
2023-02-26 01:58:51 -0500

    pupper

--- ptrack/__init__.py
+++ ptrack/__init__.py
@@ -142,7 +142,7 @@ def create_app(test_config=None):
 
             # inserted_id = vesto_col.insert_one(data).inserted_id
 
-            docs = list(vesto_col.find({"date": {"$lt": data_date}, 'version': 1}))
+            docs = list(vesto_col.find({"date": {"$lt": data_date}}))
 
             #doc_list = []
             #for doc in docs:
