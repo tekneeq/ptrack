be351fdf92378064ee3bbabd2867823424b516f3
Eugene Park <eugenepark3@gmail.com>
2023-02-28 19:39:56 -0500

    pupper

--- ptrack/__init__.py
+++ ptrack/__init__.py
@@ -183,9 +183,9 @@ def create_app(test_config=None):
         return render_template("index.html", user_image="static/jpuff.png",
                                processed_text=inserted_id)
 
-    @app.route('/line_chart', defaults={'mydate': datetime.datetime.now(timezone(datetime.timedelta(hours=-5), 'EST')).strftime('%Y-%m-%d')})
+    @app.route('/line_chart')
     @app.route('/line_chart/<mydate>')
-    def line_chart(mydate):
+    def line_chart(mydate=datetime.datetime.now(timezone(datetime.timedelta(hours=-5), 'EST')).strftime('%Y-%m-%d')):
 
         client = MongoClient(host=app.config['HOST'],
                              port=27017,
