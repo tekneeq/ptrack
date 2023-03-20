f74db1495296123343dc56c33723fd04d02ee185
Eugene Park <eugenepark3@gmail.com>
2023-03-20 15:04:46 -0400

    pupper

---
+++ tools/todai.py
@@ -0,0 +1,39 @@
+import datetime
+
+from pymongo import MongoClient
+import instance.config as config
+
+client = MongoClient(host=config.HOST,
+                     port=27017,
+                     username=config.USER,
+                     password=config.PASSWORD,
+                     tls=True,
+                     tlsAllowInvalidCertificates=True,
+                     tlsCAFile='/home/ec2-user/ptrack/ptrack/rds-combined-ca-bundle.pem',
+                     )
+db = client.vesto
+opts_col = db.opts
+
+today = datetime.datetime.today().strftime("%Y-%m-%d")
+
+
+puts = {}
+calls = {}
+opts = list(opts_col.find({"expiration_date": today, "data_date": {"$gt": today}}))
+
+for opt in opts:
+    strike_price = opt['strike_price']
+
+
+    if opt['type'] == "call":
+        if not strike_price in calls:
+            calls[strike_price] = []
+
+        calls[strike_price].append(opt)
+
+    else:
+        if not strike_price in puts:
+            puts[strike_price] = []
+        puts[strike_price].append(opt)
+
+print(f"there are {len(calls)} call strikes and {len(puts)} put strikes")
